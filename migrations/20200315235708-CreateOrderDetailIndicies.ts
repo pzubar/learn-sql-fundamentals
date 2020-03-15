8355bd36601111ef40c8d07dc84e2453fd71ce15
pzubar <pmzubar@gmail.com>
2020-03-15 23:59:44 +0200

    Adds OrderDetail Indicies migration

---
+++ migrations/20200315235708-CreateOrderDetailIndicies.ts
@@ -0,0 +1,3 @@
+import { sqlFileMigration } from '../src/migration-utils';
+
+export = sqlFileMigration('20200315235708-CreateOrderDetailIndicies');
---
+++ migrations/sqls/20200315235708-CreateOrderDetailIndicies/sqlite-up.sql
@@ -0,0 +1,2 @@
+-- Put your SQLite "up" migration here
+CREATE INDEX order_detail_product_order_id ON OrderDetail(productid, orderid)
